dd17d596824be9de5ee56509fbb8381ae7c92d4f
Aras Lamenni <manismi2me@gmail.com>
2024-03-25 08:10:24 +0700

    rename in english

--- dasar_sintaksis_pengulangan_baca_buku_dengan_while undefined count.py
+++ dasar_sintaksis_pengulangan_baca_buku_dengan_while undefined count.py
@@ -1,24 +1,24 @@
 """
 program pengulangan membaca dengan while sampai paham
 """
-jumlah_buku = 10
+book_count = 10
 print('ibu berkata, "baca semua bukumu"')
+read_count = 0
+understood_count= 0
+print(f"Jumlah buku yang sudah dibaca dan dipahami {understood_count}")
 
-jumlah_dipahami= 0
-print(f"Jumlah buku yang sudah dibaca dan dipahami {jumlah_dipahami}")
-jumlah_baca = 0
-while jumlah_baca < jumlah_buku * 2:
-    jumlah_baca = jumlah_baca + 1
-    if jumlah_dipahami == 9:
-        print(f'Buku ke {jumlah_dipahami + 1} belum paham')
+while read_count < book_count * 2:
+    read_count = read_count + 1
+    if understood_count == 9:
+        print(f'Buku ke {understood_count + 1} belum paham')
     else:
-        jumlah_dipahami = jumlah_dipahami + 1
-        print(f'Buku ke {jumlah_dipahami} sudah dibaca dan dipahami')
+        understood_count = understood_count + 1
+        print(f'Buku ke {understood_count} sudah dibaca dan dipahami')
 
-print(f'jumlah buku yang sudah dibaca dan dipahami {jumlah_dipahami}')
-if jumlah_dipahami == jumlah_buku:
+print(f'jumlah buku yang sudah dibaca dan dipahami {understood_count}')
+if understood_count == book_count:
     print('budi berkata, "bu, semua buku sudah dibaca dan dipahami"')
 else:
     print(f'Budi berkata, "Bu, tidak semua buku bisa dipahami, '
-          f'Budi hanya bisa memahami {jumlah_dipahami} buku saja.')
+          f'Budi hanya bisa memahami {understood_count} buku saja.')
 
